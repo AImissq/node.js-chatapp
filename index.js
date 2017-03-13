45cd7606e95990564d394b3e5925672f344b3745
vtruon <sp_bass88@hotmail.com>
2017-03-13 01:11:23 -0600

    Have timestamps, nicknames, and chatlog right now at least 30/100

--- index.js
+++ index.js
@@ -1,36 +1,62 @@
-//socket.io tutorial base
+//import	s
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var userNumber = 0;	
 var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
+//nicknames
+var userNumber = 1;
+var nicknames = {};
+var listOfMessages = [];
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-	  console.log('user disconnected');
-  });
+io.on('connection', function(socket)
+{
+	socket.emit('addchatlog', listOfMessages);
+	//log messages
+	console.log('a user connected');
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+	});
   
-  socket.on('chat message', function(msg){
-	  console.log('message: ' + msg);
-	  
-	  //timestamp
-	  var date = new Date();
-	  var hours = date.getHours()% 12||12;
-	  var time = hours + ":" + date.getMinutes();
-	  msg = time + " " + msg;
-	  
-	  io.emit('chat message',msg);
+	//add users
+	socket.on('adduser', function(){
+		//store username
+		myName = userNumber ++;
+		var nickname = "user" + myName;
+		socket.nickname = nickname;
+		nicknames[nickname] = nickname;
+		
+		//io.socket.emit('updateuserlist', nicknames);
+		
+	});
+	
+	//broadcast messages
+	socket.on('chat message', function(data){
+		console.log('message: ' + data);
+		  
+		//timestamp
+		var date = new Date();
+		var hours = date.getHours()
+		var ampm = (hours >= 12) ? "PM" : "AM";
+		hours = hours% 12||12;
+		var minutes =date.getMinutes();
+		minutes = (minutes <10) ? "0" + minutes:minutes;
+		var time = hours + ":" + minutes;
+		
+		data = "(" + time + ampm + ")" + " " + socket.nickname + ": " + data;
+		
+		listOfMessages.push(data);
+		
+		io.emit('chat message', data) ;
   });
-  
-
 });
 
 
-http.listen(port,function(){
+http.listen(port,function()
+{
 	console.log('listening on port', port);
 });
+
--- public/client.js
+++ public/client.js
@@ -1,12 +1,22 @@
 $(function () {
-	var socket = io();
+	var socket = io.connect('http://localhost:3000');
+	socket.emit('adduser');
 	$('form').submit(function(){
-	socket.emit('chat message', $('#m').val());
-	$('#m').val('');
-	return false;
+	
+		socket.emit('chat message', $('#m').val());
+		$('#m').val('');
+		return false;
+		
 	});
 	
-	socket.on('chat message', function(msg){
-		$('#messages').append($('<li>').text(msg));
+	socket.on('chat message', function(data){
+		$('#messages').append($('<li>').text(data)) ;
+	});
+	
+	socket.on('addchatlog', function(listOfMessages){
+		for (var i = 0; i <listOfMessages.length; i ++)
+		{
+			$('#messages').append($('<li>').text(listOfMessages[i])) ;
+		}
 	});
 });
