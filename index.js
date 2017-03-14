d5df9a6760c8eaa122dfac52672ca1a6c56f8f2b
vtruon <sp_bass88@hotmail.com>
2017-03-13 23:15:44 -0600

    working on bold and color

--- index.js
+++ index.js
@@ -11,79 +11,88 @@ app.use(express.static(__dirname + '/public'));
 var userNumber = 1;
 var nicknames = [];
 
+//list of users
+var listOfUsers = [];
 //message history
 var listOfMessages = [];
 
 io.on('connection', function(socket)
 {
-	socket.emit('addchatlog', listOfMessages);
 	//log messages
 	console.log('a user connected');
 	socket.on('disconnect', function(){
 		console.log('user disconnected');
 	});
   
-	//add users
-	//first thing done, after adding chat log if any, by every client
+  
+	//add user
+	//first thing done
 	socket.on('adduser', function(){
-		//store username
+		//create store username
 		myName = userNumber ++;
 		var nickname = "user" + myName;
 		socket.nickname = nickname;
 		nicknames.push(nickname);
 		
+		//add to user list and tell clients to update their html
 		//io.socket.emit('updateuserlist', nicknames);
+		
+		//update chat log if any messages
+		socket.emit('addchatlog', listOfMessages);
 		
 	});
 	
 	//broadcast messages
-	socket.on('chat message', function(data){
+	socket.on('message', function(data)
+	{
 		console.log('message: ' + data);
-		  
-		  
-		//get timestamp and format it
+		
+		var time = getCurrentTime();
+		
+		//data = time  + " " + socket.nickname + ": " + data;
+		
+		//add message to log list
+		listOfMessages.push(time  + " " + socket.nickname + ": " + data);
+		
+		//send regular message
+		io.emit('message', {type: 'chat', nickname: socket.nickname, message:data, timestamp: time}) ;
+		
+	});
+	
+	//check if user entered change nick command, if so change nick, otherwise send message
+	socket.on('changenickname',function(data,callback)
+	{
+		//if nickname already taken, callback false to display error message
+		if(nicknames.indexOf(data)!= -1)
+		{
+			callback(false);
+		}
+		
+		//else change nickname
+		else
+		{
+			callback(true);
+			var oldnickname =socket.nickname;
+			nicknames[ nicknames.indexOf(socket.nickname)] = data;//update list with new nickname
+			socket.nickname=data;
+			
+			//notice message
+			io.emit('message', {type: 'notice' , message:oldnickname + " changed nickname to " + socket.nickname}) ;
+		}
+		
+	});
+});
+
+function getCurrentTime(){
+	//get timestamp and format it
 		var date = new Date();
 		var hours = date.getHours()
 		var ampm = (hours >= 12) ? "PM" : "AM";
 		hours = hours% 12||12;
 		var minutes =date.getMinutes();
 		minutes = (minutes <10) ? "0" + minutes:minutes;
-		var time = "(" + hours + ":" + minutes + ampm + ")";
-		
-		var messageAsArray = data.split(" ");
-		data = time  + " " + socket.nickname + ": " + data;
-		
-		listOfMessages.push(data);
-		
-		//check if user entered change nick command, if so change nick, otherwise send message
-		
-		if (messageAsArray[0] === "/nick")
-		{
-			if(nicknames.indexOf(messageAsArray[1])	== -1)
-			{
-				nicknames[ nicknames.indexOf(socket.nickname)] = messageAsArray[1]//update list with new nickname
-				socket.nickname=messageAsArray[1];
-				
-			}
-			
-			else
-			{
-				data = "Error: Username already taken, pick a different one.";
-				io.emit('chat message', data) ;
-			}
-		}
-		
-		else
-		{	//send regular message
-			io.emit('chat message', data) ;
-		}
-		
-		
-		
-  });
-});
-
-
+		return "(" + hours + ":" + minutes + ampm + ")";
+}
 http.listen(port,function()
 {
 	console.log('listening on port', port);
--- public/client.js
+++ public/client.js
@@ -1,16 +1,9 @@
-$(function () {
+/*$(function () {
 	var socket = io.connect('http://localhost:3000');
-	socket.emit('adduser');
-	$('form').submit(function(){
 	
-		socket.emit('chat message', $('#m').val());
-		$('#m').val('');
-		return false;
-		
-	});
 	
-	socket.on('chat message', function(data){
-		$('#messages').append($('<li>').text(data)) ;
+	socket.on('connect',function(){
+		socket.emit('adduser');
 	});
 	
 	socket.on('addchatlog', function(listOfMessages){
@@ -18,5 +11,80 @@ $(function () {
 		{
 			$('#messages').append($('<li>').text(listOfMessages[i])) ;
 		}
+	});
+	
+	$('form').submit(function(){
+	
+		socket.emit('message', $('#m').val());
+		$('#m').val('');
+		return false;
+		
+	});
+	
+	socket.on('message', function(data){
+		$('#messages').append($('<li>').text(data)) ;
+	});
+	
+	
+});*/
+
+jQuery(function($){
+	var socket=io.connect();
+	var $messageForm = $('form');
+	var $messageBox = $('#m');
+	var $chat = $('#messages');
+	var myUsername;
+	//add new user
+	socket.on('connect',function(){
+		socket.emit('adduser');
+	});
+	
+	socket.on('addchatlog', function(listOfMessages){
+		for (var i = 0; i <listOfMessages.length; i ++)
+		{
+			$chat.append($('<li>').text(listOfMessages[i])) ;
+		}
+	});
+	
+	$messageForm.submit(function(e){
+		e.preventDefault();
+		
+		var messageAsArray = $messageBox.val().split(" ");
+		
+		//if message is nickchange
+		if(messageAsArray[0]=== "/nick")
+		{
+			socket.emit('changenickname', messageAsArray[1], function(data){
+				if(!data){
+					$chat.append($('<li>').text("Error: Username already taken.")) ;
+				}
+			});
+		}
+		
+		//if regular message
+		else{
+			socket.emit('message', $messageBox.val());
+		}
+		$messageBox.val('');
+		
+	});
+	
+	//message function
+	//depending on type of message, and who sent it, colouring, and bold will be different.
+	socket.on('message', function(data){
+		if (data.type === 'chat'){
+			//if current user sent it
+			if(data.nickname === "user3"){
+				$chat.append($('<li>').text(socket.nickname));
+			}
+			else{
+				$chat.append($('<li>').text(data.timestamp  + " " + data.nickname + ": " + data.message)); 
+			}
+		}
+		
+		else if (data.type ==='notice'){
+			$chat.append($('<li>').text(data.message)) ;
+		}
+		
 	});
 });
