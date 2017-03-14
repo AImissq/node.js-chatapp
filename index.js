9cb338624717fd79b7858663d17832d053bddbb6
vtruon <sp_bass88@hotmail.com>
2017-03-13 20:36:12 -0600

    Last working code before JQuery change

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ app.use(express.static(__dirname + '/public'));
 
 //nicknames
 var userNumber = 1;
-var nicknames = {};
+var nicknames = [];
+
+//message history
 var listOfMessages = [];
 
 io.on('connection', function(socket)
@@ -22,12 +24,13 @@ io.on('connection', function(socket)
 	});
   
 	//add users
+	//first thing done, after adding chat log if any, by every client
 	socket.on('adduser', function(){
 		//store username
 		myName = userNumber ++;
 		var nickname = "user" + myName;
 		socket.nickname = nickname;
-		nicknames[nickname] = nickname;
+		nicknames.push(nickname);
 		
 		//io.socket.emit('updateuserlist', nicknames);
 		
@@ -37,20 +40,46 @@ io.on('connection', function(socket)
 	socket.on('chat message', function(data){
 		console.log('message: ' + data);
 		  
-		//timestamp
+		  
+		//get timestamp and format it
 		var date = new Date();
 		var hours = date.getHours()
 		var ampm = (hours >= 12) ? "PM" : "AM";
 		hours = hours% 12||12;
 		var minutes =date.getMinutes();
 		minutes = (minutes <10) ? "0" + minutes:minutes;
-		var time = hours + ":" + minutes;
+		var time = "(" + hours + ":" + minutes + ampm + ")";
 		
-		data = "(" + time + ampm + ")" + " " + socket.nickname + ": " + data;
+		var messageAsArray = data.split(" ");
+		data = time  + " " + socket.nickname + ": " + data;
 		
 		listOfMessages.push(data);
 		
-		io.emit('chat message', data) ;
+		//check if user entered change nick command, if so change nick, otherwise send message
+		
+		if (messageAsArray[0] === "/nick")
+		{
+			if(nicknames.indexOf(messageAsArray[1])	== -1)
+			{
+				nicknames[ nicknames.indexOf(socket.nickname)] = messageAsArray[1]//update list with new nickname
+				socket.nickname=messageAsArray[1];
+				
+			}
+			
+			else
+			{
+				data = "Error: Username already taken, pick a different one.";
+				io.emit('chat message', data) ;
+			}
+		}
+		
+		else
+		{	//send regular message
+			io.emit('chat message', data) ;
+		}
+		
+		
+		
   });
 });
 
