1585b6403fd75a75dd9c6c02c8bf4d6930c259b6
vtruon <sp_bass88@hotmail.com>
2017-03-14 21:18:38 -0600

    Done align to bottom and vertical, only thing left is cookies, maybe resizing and maybe presentation, cookies priority

--- public/client.js
+++ public/client.js
@@ -7,6 +7,7 @@ jQuery(function($){
 	var $currentUserName = $('#usernameIndicator');
 	var myUsername;
 	var myNickColor = 000000;
+
 	
 	//add new user
 	socket.on('connect',function(){
@@ -22,6 +23,7 @@ jQuery(function($){
 		for (var i = 0; i <listOfMessages.length; i ++)
 		{
 			$chat.append($('<li>').text(listOfMessages[i])) ;
+			updateScroll();
 		}
 	});
 	
@@ -41,32 +43,37 @@ jQuery(function($){
 		
 		var messageAsArray = $messageBox.val().split(" ");
 		
-		//if message is nickchange
-		if(messageAsArray[0]=== "/nick" && messageAsArray.length>1)
-		{
-			socket.emit('changenickname', messageAsArray[1], function(data){
-				if(!data){
-					$chat.append($('<li>').text("Error: Username already taken.")) ;
-				}
-				
-				//set private username to new username
-				else{
-					myUsername = data;
-					$currentUserName.html('<h2>' + myUsername + '</h2>');
-				}
-			});
-		}
-		
-		//if change nickname color
-		else if(messageAsArray[0]=== "/nickcolor" && messageAsArray.length>1)
-		{
-			socket.emit('changenicknameColor', messageAsArray[1]);
-			myNickColor = messageAsArray[1];
-		}
-		
-		//if regular message
-		else{
-			socket.emit('message',{type: 'chat', message:$messageBox.val(), nickColor: myNickColor} );
+		//if there is text
+		if(messageAsArray.length>0)
+		{	
+			//if message is nickchange
+			if(messageAsArray[0]=== "/nick" && messageAsArray.length>1)
+			{
+				socket.emit('changenickname', messageAsArray[1], function(data){
+					if(!data){
+						$chat.append($('<li>').text("Error: Username already taken.")) ;
+						updateScroll();
+					}
+					
+					//set private username to new username
+					else{
+						myUsername = data;
+						$currentUserName.html('<h2>' + myUsername + '</h2>');
+					}
+				});
+			}
+			
+			//if change nickname color
+			else if(messageAsArray[0]=== "/nickcolor" && messageAsArray.length>1)
+			{
+				socket.emit('changenicknameColor', messageAsArray[1]);
+				myNickColor = messageAsArray[1];
+			}
+			
+			//if regular message
+			else if(messageAsArray[0].charAt(0)!= '/'){
+				socket.emit('message',{type: 'chat', message:$messageBox.val(), nickColor: myNickColor} );
+			}
 		}
 		$messageBox.val('');
 		
@@ -79,18 +86,61 @@ jQuery(function($){
 			//if current user sent it
 			if(data.nickname === myUsername){
 				$chat.append($('<li>').html((data.timestamp  + " " + data.nickname.fontcolor(myNickColor) + ": " + data.message).bold())); 
+				updateScroll();
 			}
 			else{
 				$chat.append($('<li>').html(data.timestamp  + " " + data.nickname.fontcolor(data.nickColor) + ": " + data.message)); 
+				updateScroll();
 			}
 		}
 		
 		else if (data.type ==='notice'){
 			$chat.append($('<li>').html(data.message.fontcolor("ff0000"))) ;
+			updateScroll();
 		}
 		
 	});
 	
 	
 });
+
+function updateScroll(){
+    var myDiv = document.getElementById("messageArea");
+    myDiv.scrollTop = myDiv.scrollHeight;
+}
+
+//https://www.w3schools.com/js/js_cookies.asp
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user = getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+        user = prompt("Please enter your name:", "");
+        if (user != "" && user != null) {
+            setCookie("username", user, 365);
+        }
+    }
+}
 	
